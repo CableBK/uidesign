ab5e8c6cab73d5fe7ea77c680f55bbe9973b6c7c
เคเบิล <cbkcontactmail@gmail.com>
2024-09-06 22:32:01 +0700

    Update

---
+++ uidesign/lib/pages/team_folder.dart
@@ -0,0 +1,48 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class TeamFolderPage extends StatefulWidget {
+  const TeamFolderPage({super.key});
+
+  @override
+  State<TeamFolderPage> createState() => _TeamFolderPageState();
+}
+
+class _TeamFolderPageState extends State<TeamFolderPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[100],
+        body: Column(
+          children: [
+            Container(
+              height: 170,
+              decoration: BoxDecoration(color: Colors.blue.shade800),
+              child: Row(
+                children: [
+                  Column(
+                    children: [
+                      Text(
+                        'CableBK',
+                        style: TextStyle(
+                            fontSize: 26,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      Text(
+                        'Team folder',
+                        style: TextStyle(
+                            fontSize: 26,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+}
