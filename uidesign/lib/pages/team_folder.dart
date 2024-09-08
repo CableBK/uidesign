26bfc0296870e9c153557366be20129cbeca5a77
เคเบิล <cbkcontactmail@gmail.com>
2024-09-08 23:00:52 +0700

    Update team_folder.dart

--- uidesign/lib/pages/team_folder.dart
+++ uidesign/lib/pages/team_folder.dart
@@ -18,12 +18,16 @@ class _TeamFolderPageState extends State<TeamFolderPage> {
           children: [
             Container(
               padding: EdgeInsets.symmetric(vertical: 25, horizontal: 25),
+              alignment: Alignment.bottomCenter,
               height: 170,
               decoration: BoxDecoration(color: Colors.blue.shade800),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Column(
-                    children: [
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
                       Text(
                         'CableBK',
                         style: TextStyle(
@@ -34,14 +38,58 @@ class _TeamFolderPageState extends State<TeamFolderPage> {
                       Text(
                         'Team folder',
                         style: TextStyle(
-                            fontSize: 26,
-                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            fontWeight: FontWeight.normal,
                             color: Colors.white),
                       ),
                     ],
-                  )
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: Colors.black.withOpacity(.1)),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.search,
+                            size: 28,
+                            color: Colors.white,
+                          ),
+                          onPressed: () => null,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: Colors.black.withOpacity(.1)),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.notifications,
+                            size: 28,
+                            color: Colors.white,
+                          ),
+                          onPressed: () => null,
+                        ),
+                      )
+                    ],
+                  ),
                 ],
               ),
+            ),
+            Row(
+              children: [
+                RichText(
+                    text: const TextSpan(
+                        text: 'Storage',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold)))
+              ],
             )
           ],
         ));
