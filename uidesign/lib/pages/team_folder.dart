3aaacb8bbca8e9df97c4c54ca2877303d52ae8e2
เคเบิล <cbkcontactmail@gmail.com>
2024-09-09 12:00:44 +0700

    Update team_folder.dart

--- uidesign/lib/pages/team_folder.dart
+++ uidesign/lib/pages/team_folder.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
@@ -80,15 +80,64 @@ class _TeamFolderPageState extends State<TeamFolderPage> {
                 ],
               ),
             ),
-            Row(
-              children: [
-                RichText(
+            SizedBox(
+              height: 25,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  RichText(
                     text: const TextSpan(
                         text: 'Storage',
                         style: TextStyle(
                             color: Colors.black,
                             fontSize: 18,
-                            fontWeight: FontWeight.bold)))
+                            fontWeight: FontWeight.bold),
+                        children: [
+                          TextSpan(
+                            text: '9.1/10GB',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w300,
+                              fontSize: 16,
+                            ),
+                          )
+                        ]),
+                  ),
+                  const Text(
+                    'Upgrade',
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 4,
+                      color: Colors.blue,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    const Text(
+                      'SOURCE',
+                      style:
+                          TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                )
               ],
             )
           ],
