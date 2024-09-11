77cf4ccf44e21cb8e76db1917057a47bb6a94164
เคเบิล <cbkcontactmail@gmail.com>
2024-09-11 22:37:36 +0700

    Update team_folder.dart

--- uidesign/lib/pages/team_folder.dart
+++ uidesign/lib/pages/team_folder.dart
@@ -16,203 +16,204 @@ class _TeamFolderPageState extends State<TeamFolderPage> {
   Widget build(BuildContext context) {
     availableScreenWidth = MediaQuery.of(context).size.width - 50;
     return Scaffold(
-        backgroundColor: Colors.grey[100],
-        body: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 25, horizontal: 25),
-              alignment: Alignment.bottomCenter,
-              height: 170,
-              decoration: BoxDecoration(color: Colors.blue.shade800),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text(
-                        'CableBK',
-                        style: TextStyle(
-                            fontSize: 26,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
-                      ),
-                      Text(
-                        'Team folder',
-                        style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.normal,
-                            color: Colors.white),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            color: Colors.black.withOpacity(.1)),
-                        child: IconButton(
-                          icon: Icon(
-                            Icons.search,
-                            size: 28,
-                            color: Colors.white,
-                          ),
-                          onPressed: () => null,
-                        ),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            color: Colors.black.withOpacity(.1)),
-                        child: IconButton(
-                          icon: Icon(
-                            Icons.notifications,
-                            size: 28,
-                            color: Colors.white,
-                          ),
-                          onPressed: () => null,
-                        ),
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  RichText(
-                    text: const TextSpan(
-                        text: 'Storage ',
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold),
-                        children: [
-                          TextSpan(
-                            text: '9.1/10GB',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.w300,
-                              fontSize: 16,
-                            ),
-                          )
-                        ]),
-                  ),
-                  const Text(
-                    'Upgrade',
-                    style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
-                  )
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25),
-              child: Row(
-                children: [
-                  buildFileSizeChart('SOURCE', Colors.blue, .3),
-                  const SizedBox(
-                    width: 2,
-                  ),
-                  buildFileSizeChart('DOC', Colors.red, .25),
-                  const SizedBox(
-                    width: 2,
-                  ),
-                  buildFileSizeChart('IMAGES', Colors.yellow, .20),
-                  const SizedBox(
-                    width: 2,
-                  ),
-                  buildFileSizeChart('', Colors.grey[200], .23),
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            const Divider(
-              height: 20,
-            ),
-            Expanded(
-              child: ListView(
-                padding: EdgeInsets.all(25),
-                children: [
-                  const Text(
-                    'Recently updated',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
+      backgroundColor: Colors.grey[100],
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 25, horizontal: 25),
+            alignment: Alignment.bottomCenter,
+            height: 170,
+            decoration: BoxDecoration(color: Colors.blue.shade800),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'CableBK',
+                      style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
                     ),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Row(
-                    children: [
-                      buildFileColumn('P001', 'CableBK', '.folder'),
-                      SizedBox(
-                        width: availableScreenWidth * .03,
+                    Text(
+                      'Team folder',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.black.withOpacity(.1)),
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.search,
+                          size: 28,
+                          color: Colors.white,
+                        ),
+                        onPressed: () => null,
                       ),
-                      buildFileColumn('P001', 'Flutter exercise', '.folder'),
-                      SizedBox(
-                        width: availableScreenWidth * .03,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.black.withOpacity(.1)),
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.notifications,
+                          size: 28,
+                          color: Colors.white,
+                        ),
+                        onPressed: () => null,
                       ),
-                      buildFileColumn('P001', 'ฝึกทำ UI', '.folder'),
-                    ],
-                  ),
-                  Divider(
-                    height: 60,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text(
-                        'Project',
-                        style: TextStyle(
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                RichText(
+                  text: const TextSpan(
+                      text: 'Storage ',
+                      style: TextStyle(
                           color: Colors.black,
                           fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        'Create New',
-                        style: TextStyle(
-                          color: Colors.blue,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
+                          fontWeight: FontWeight.bold),
+                      children: [
+                        TextSpan(
+                          text: '9.1/10GB',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w300,
+                            fontSize: 16,
+                          ),
+                        )
+                      ]),
+                ),
+                const Text(
+                  'Upgrade',
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Row(
+              children: [
+                buildFileSizeChart('SOURCE', Colors.blue, .3),
+                const SizedBox(
+                  width: 2,
+                ),
+                buildFileSizeChart('DOC', Colors.red, .25),
+                const SizedBox(
+                  width: 2,
+                ),
+                buildFileSizeChart('IMAGES', Colors.yellow, .20),
+                const SizedBox(
+                  width: 2,
+                ),
+                buildFileSizeChart('', Colors.grey[200], .23),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          const Divider(
+            height: 20,
+          ),
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.all(25),
+              children: [
+                const Text(
+                  'Recently updated',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
                   ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  buildProjectView('Kabel'),
-                  buildProjectView('Time'),
-                  buildProjectView('Cable'),
-                  buildProjectView('Git'),
-                  buildProjectView('Github'),
-                  buildProjectView('MOVE EV X'),
-                ],
-              ),
-            )
-          ],
-        ));
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  children: [
+                    buildFileColumn('P001', 'CableBK', '.folder'),
+                    SizedBox(
+                      width: availableScreenWidth * .03,
+                    ),
+                    buildFileColumn('P001', 'Flutter exercise', '.folder'),
+                    SizedBox(
+                      width: availableScreenWidth * .03,
+                    ),
+                    buildFileColumn('P001', 'ฝึกทำ UI', '.folder'),
+                  ],
+                ),
+                Divider(
+                  height: 60,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      'Project',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Create New',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                buildProjectView('Kabel'),
+                buildProjectView('Time'),
+                buildProjectView('Cable'),
+                buildProjectView('Git'),
+                buildProjectView('Github'),
+                buildProjectView('MOVE EV X'),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 
   Container buildProjectView(String folderName) {
